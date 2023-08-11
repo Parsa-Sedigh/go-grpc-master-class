c4d91eb399f33a285240088ca927a1d1223aa560
parsa <parsa.sedigh78@gmail.com>
2023-08-11 22:04:10 +0330

    build: update generate.sh to output grpc generated code

--- generate.sh
+++ generate.sh
@@ -2,10 +2,11 @@
 
 # this doesn't work:
 # protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
-#protoc ./greet/greetpb/greet.proto --go-grpc_out=.
+# protoc ./greet/greetpb/greet.proto --go-grpc_out=.
 protoc --proto_path=greet/greetpb --go_out=greet/greetpb --go-grpc_out=. --go_opt=paths=source_relative greet.proto
 
 #protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
 protoc calculator/calculatorpb/calculator.proto --go-grpc_out=.
 
-protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
+#protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
+protoc --proto_path=blog/blogpb --go_out=blog/blogpb --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=. --go_opt=paths=source_relative blog.proto
